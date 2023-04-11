8b363f547533a08299de8f4d5c490df4516e329c
Yannick Vessaz <vessaz@epsitec.ch>
2023-04-11 15:01:20 +0200

    Add initial types

---
+++ base-types.js
@@ -0,0 +1,309 @@
+// @ts-check
+
+/**
+ * Base class for any type definition
+ * @template T
+ */
+class Type {
+  /**
+   * Type parameter T must be used somewhere
+   * so it is assigned to this empty private field
+   * @type {T}
+   */
+  #type;
+
+  /**
+   * @param {string} name
+   */
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+/**
+ * @extends {Type<any>}
+ */
+class AnyType extends Type {
+  constructor() {
+    super("any");
+  }
+}
+
+/**
+ * Get the JS type of a base type definition
+ * @template {AnyType} T
+ * @typedef {T extends Type<infer U> ? U : never} jsType
+ */
+
+// Alternative method that doesn't work if #type is private
+// /**
+//  * @template {AnyType} T
+//  * @typedef {T["_type"]} jsType
+//  */
+
+/**
+ * @typedef {{[k: string]: any}} ObjectShape
+ */
+
+// Variant that does not work if the shape is a class
+// /**
+//  * @typedef {{[k: string]: AnyType}} ObjectShape
+//  */
+
+/**
+ * @typedef {abstract new (...args: any) => ObjectShape} ClassShape
+ */
+
+// ## Type helpers ##
+
+/**
+ * @template T
+ * @typedef {T} identity
+ */
+
+/**
+ * @template T
+ * @typedef {identity<{[k in keyof T]: T[k]}>} flatten
+ */
+
+/**
+ * Get the JS type of an object shape.
+ * Note: Using "identity" is needed to display JS object types
+ * and not a combination of other types.
+ * @template {ObjectShape} S
+ * @typedef {identity<{[k in keyof S]: t<S[k]>}>} objectType
+ */
+
+/**
+ * Get the JS type of a type defined as a class.
+ * @template {ClassShape} T
+ * @typedef {objectType<InstanceType<T>>} classType
+ */
+
+/**
+ * @typedef {AnyType | ClassShape | ObjectShape} AnyTypeOrShape
+ */
+
+/**
+ * Get the JS type of a complexe type definition.
+ * @template T
+ * @typedef {T extends AnyType ? jsType<T> : T extends ClassShape ? classType<T> : T extends ObjectShape ? objectType<T> : never} t
+ */
+
+// Variant constrained to AnyTypeOrShape but more complicated to import
+// /**
+//  * Get the JS type of a complexe type definition.
+//  * @template {AnyTypeOrShape} T
+//  * @typedef {T extends AnyType ? jsType<T> : T extends ClassShape ? classType<T> : objectType<T>} t
+//  */
+
+// ## Type classes ##
+
+/**
+ * @extends {Type<boolean>}
+ */
+class BooleanType extends Type {
+  constructor() {
+    super("boolean");
+  }
+}
+
+/**
+ * @extends {Type<string>}
+ */
+class StringType extends Type {
+  constructor() {
+    super("string");
+  }
+}
+
+/**
+ * @extends {Type<number>}
+ */
+class NumberType extends Type {
+  constructor() {
+    super("number");
+  }
+}
+
+/**
+ * @template T
+ * @extends {Type<T>}
+ */
+class ValueType extends Type {
+  /** @param {T} value */
+  constructor(value) {
+    super(`value<${value}>`);
+    this.value = value;
+  }
+}
+
+/**
+ * @template {AnyTypeOrShape} T
+ * @extends {Type<t<T> | undefined>}
+ */
+class OptionType extends Type {
+  /** @param {T} subType */
+  constructor(subType) {
+    super(`option<${subType.name}>`);
+    this.subType = subType;
+  }
+}
+
+/**
+ * @template {AnyTypeOrShape} T
+ * @extends {Type<Array<t<T>>>}
+ */
+class ArrayType extends Type {
+  /** @param {T} valuesType */
+  constructor(valuesType) {
+    super(`array<${valuesType.name}>`);
+    this.valuesType = valuesType;
+  }
+}
+
+/**
+ * @template {ObjectShape} T
+ * @extends {Type<objectType<T>>}
+ */
+class ObjectType extends Type {
+  /**
+   * @param {string} name
+   * @param {T} properties
+   */
+  constructor(name, properties) {
+    super(name);
+    this.properties = properties;
+  }
+}
+
+/**
+ * @template {readonly any[]} T
+ * @extends {Type<T[number]>}
+ */
+class EnumerationType extends Type {
+  /**
+   * @param {T} values
+   */
+  constructor(values) {
+    super("enumeration");
+    this.values = values;
+  }
+}
+
+/**
+ * @template {AnyTypeOrShape[]} T
+ * @extends {Type<t<T[number]>>}
+ */
+class UnionType extends Type {
+  /**
+   * @param {T} types
+   */
+  constructor(types) {
+    super("union");
+    this.types = types;
+  }
+}
+
+/**
+ * @template {AnyTypeOrShape} K
+ * @template {AnyTypeOrShape} V
+ * @extends {Type<Map<t<K>, t<V>>>}
+ */
+class MapType extends Type {
+  /**
+   * @param {K} keysType
+   * @param {V} valuesType
+   */
+  constructor(keysType, valuesType) {
+    super(`map<${keysType.name},${valuesType.name}>`);
+    this.keysType = keysType;
+    this.valuesType = valuesType;
+  }
+}
+
+// ## Type instances ##
+
+const any = new AnyType();
+
+const boolean = new BooleanType();
+
+const string = new StringType();
+
+const number = new NumberType();
+
+// This notation does not support "const" parameter
+// /**
+//  * @template  T
+//  * @param {T} value
+//  * @returns {ValueType<T>}
+//  */
+/** @type {<const T>(value: T) => ValueType<T>} */
+const value = (value) => new ValueType(value);
+
+/**
+ * @template {AnyTypeOrShape} T
+ * @param {T} subType
+ * @returns {OptionType<T>}
+ */
+const option = (subType) => new OptionType(subType);
+
+/**
+ * @template {AnyTypeOrShape} T
+ * @param {T} subType
+ * @returns {ArrayType<T>}
+ */
+const array = (subType) => new ArrayType(subType);
+
+/**
+ * @template {ObjectShape} T
+ * @param {T} properties
+ * @returns {ObjectType<T>}
+ */
+const object = (properties) => new ObjectType("object", properties);
+
+/** @type {<const T extends readonly unknown[]>(...values: T) => EnumerationType<T>} */
+const enumeration = (...values) => new EnumerationType(values);
+
+/**
+ * @template {AnyTypeOrShape[]} T
+ * @param {T} types
+ * @returns {UnionType<T>}
+ */
+const union = (...types) => new UnionType(types);
+
+/**
+ * @template {AnyTypeOrShape} K
+ * @template {AnyTypeOrShape} V
+ * @param {K} keysType
+ * @param {V} valuesType
+ * @returns {MapType<K,V>}
+ */
+const map = (keysType, valuesType) => new MapType(keysType, valuesType);
+
+module.exports = {
+  Type,
+  AnyType,
+  BooleanType,
+  StringType,
+  NumberType,
+  ValueType,
+  OptionType,
+  ArrayType,
+  ObjectType,
+  EnumerationType,
+  UnionType,
+  MapType,
+
+  any,
+  boolean,
+  string,
+  number,
+  value,
+  option,
+  array,
+  object,
+  enumeration,
+  union,
+  map,
+};
---
+++ example.js
@@ -0,0 +1,224 @@
+// @ts-check
+const {
+  value,
+  string,
+  number,
+  option,
+  array,
+  object,
+  map,
+  enumeration,
+  union,
+} = require("./base-types.js");
+/**
+ * @template T
+ * @typedef {import("./base-types.js").t<T>} t
+ */
+
+example1: {
+  const NameType = string;
+
+  /** @type {t<typeof NameType>} */
+  let myName = "toto";
+
+  /** @type {t<typeof NameType>} */
+  let wrongName = 2; // error
+
+  /** @type {t<typeof number>} */
+  let myNum = 42;
+
+  const MayBeNameType = option(NameType);
+  const MayBeNameType2 = option(option(option(NameType)));
+
+  /** @type {t<typeof MayBeNameType>} */
+  let myName2 = "toto";
+
+  /** @type {t<typeof MayBeNameType2>} */
+  let myName3 = "toto";
+
+  const NameListType = array(NameType);
+
+  /** @type {t<typeof NameListType>} */
+  let names = ["toto", "tata", "titi"];
+  /** @type {t<typeof NameListType>} */
+  let wrongNames = [1, "toto"]; // error
+}
+
+example2: {
+  const NameType = string;
+
+  const UserType = object({
+    name: NameType,
+    age: number,
+  });
+
+  /** @type {t<typeof UserType>} */
+  let toto = {
+    name: "Toto",
+    age: 12,
+  };
+
+  class User {
+    /** @param {t<typeof UserType>} obj */
+    constructor(obj) {
+      this.name = obj.name;
+      this.age = obj.age;
+    }
+  }
+
+  /** @typedef {t<typeof UserType>} User3 */
+
+  let tata = new User({
+    name: "Tata",
+    age: 11,
+  });
+
+  function User2(obj) {
+    return new User(obj);
+  }
+
+  let titi = User2({
+    name: "Titi",
+    age: 11,
+  });
+
+  let users = [toto, tata, titi];
+  let user = users.find((user) => user.name === "Toto");
+  console.log(user.name); // user could be undefined
+}
+
+example3: {
+  let AddressType = object({
+    postalCode: number,
+    townName: string,
+  });
+
+  let NameAndAddressType = object({
+    name: string,
+    address: AddressType,
+  });
+
+  /** @type {t<typeof NameAndAddressType>} */
+  let test = {
+    name: "Toto",
+    address: {
+      // postalCode: 1000,
+      postalCode: "1000", // error
+      townName: "Lausanne",
+    },
+  };
+}
+
+example4: {
+  class AddressType {
+    postalCode = number;
+    townName = string;
+  }
+
+  class NameAndAddressType {
+    name = string;
+    address = AddressType;
+  }
+
+  class Address {
+    /** @param {t<AddressType>} obj */
+    constructor(obj) {
+      this.postalCode = obj.postalCode;
+      this.townName = obj.townName;
+    }
+  }
+
+  class NameAndAddress {
+    /** @param {t<NameAndAddressType>} obj */
+    constructor(obj) {
+      this.name = obj.name;
+      this.address = new Address(obj.address);
+    }
+  }
+
+  const test = new NameAndAddress({
+    name: "Toto",
+    address: {
+      // postalCode: 1000,
+      postalCode: "1000", // error
+      townName: "Lausanne",
+    },
+  });
+}
+
+example5: {
+  class CircleType {
+    name = value("circle");
+    radius = number;
+  }
+
+  /** @type {t<typeof CircleType>} */
+  let circle1 = {
+    name: "circle",
+    radius: 2,
+  };
+
+  circle1.name = "square"; // error
+
+  /** @type {t<typeof CircleType>} */
+  let circle2 = {
+    name: "circle",
+    radius: 100,
+  };
+
+  // Array
+
+  const CirclesArray = array(CircleType);
+
+  /** @type {t<typeof CirclesArray>} */
+  let circleList = [circle1, circle2];
+
+  /** @type {t<typeof CirclesArray>} */
+  let circleListError = [{ name: "square", radius: "2" }]; // error
+
+  // Option
+
+  const MaybeCircleType = option(CircleType);
+
+  /** @type {t<typeof MaybeCircleType>} */
+  let optionalCircle = undefined;
+  optionalCircle = circle1;
+
+  // Enum
+
+  const ShapeNameType = enumeration("circle", "square", "triangle");
+
+  /** @type {t<typeof ShapeNameType>} */
+  let shapeName = "circle";
+  shapeName = "square";
+  shapeName = "wrong"; // error
+
+  // Union
+
+  class SquareType {
+    name = value("square");
+    length = number;
+  }
+
+  let square1 = {
+    name: "square",
+    length: 10,
+  };
+
+  const ShapeType = union(CircleType, SquareType);
+
+  /** @type {t<typeof ShapeType>} */
+  let shape = circle1;
+  shape = square1; // error
+
+  // Map
+
+  const CirclesMap = map(string, CircleType);
+
+  /** @type {t<typeof CirclesMap>} */
+  let kindOfCircles = new Map();
+  kindOfCircles.set("small", circle1);
+  kindOfCircles.set("large", circle2);
+
+  kindOfCircles.set("error", square1); // error
+}
