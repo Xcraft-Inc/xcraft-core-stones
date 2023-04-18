abd8c2d82d4658cfe8509e10568fd8aa73f4c087
Yannick Vessaz <vessaz@epsitec.ch>
2023-04-18 14:35:05 +0200

    Improve validation example

--- examples/validation.js
+++ examples/validation.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-labels */
 // @ts-check
 
-const {number, string} = require('../base-types.js');
+const {number, string, array, option} = require('../base-types.js');
 const parse = require('../parse.js');
 const validate = require('../validate.js');
 
@@ -17,21 +17,27 @@ let obj = {
 };
 
 example1: {
+  console.log('# Example 1');
   if (validate(obj, UserType)) {
     obj; // type {name: string, age: number}
+    console.log('The object is an user');
   } else {
     obj; // type any
+    console.log('The object is not an user');
   }
 }
 
 example2: {
+  console.log('# Example 2');
   let user = parse(obj, UserType);
   user; // type {name: string, age: number}
   user.name = 'tata';
+  console.log('User name is', user.name);
   user.age = '11'; // error
 }
 
 example3: {
+  console.log('# Example 3');
   try {
     /** @type {any} */
     let obj2 = {
@@ -41,6 +47,43 @@ example3: {
 
     let user2 = parse(obj2, UserType); // throws error
     user2; // type {name: string, age: number}
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+example4: {
+  console.log('# Example 4');
+
+  const UserList = array(UserType);
+
+  class UserGroup {
+    name = string;
+    description = option(string);
+    users = UserList;
+  }
+
+  /** @type {any} */
+  const group = {
+    name: 'Comic book readers',
+    users: [
+      {
+        name: 'Toto',
+        age: 12,
+      },
+      {
+        name: 'Tata',
+        age: 11,
+      },
+    ],
+  };
+  let test1 = parse(group, UserGroup);
+  console.log('Ok');
+
+  test1.users[1].age = '11'; // wrong
+  try {
+    let test2 = parse(group, UserGroup); // throws error
+    test2;
   } catch (err) {
     console.log(err.message);
   }
