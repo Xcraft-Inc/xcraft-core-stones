c7ce8aaeb53c16cd75106becae5a265302faac79
Yannick Vessaz <vessaz@epsitec.ch>
2024-02-15 16:12:12 +0100

    Add typdefs file to use types without complicated import

---
+++ typedefs.js
@@ -0,0 +1,31 @@
+/**
+ * @typedef {import("./base-types.js").AnyTypeOrShape} AnyTypeOrShape
+ * @typedef {import('./base-types.js').AnyType} AnyType
+ * @typedef {import('./base-types.js').ClassType} ClassType
+ * @typedef {import("./base-types.js").ClassShape} ClassShape
+ * @typedef {import("./base-types.js").ObjectShape} ObjectShape
+ */
+/**
+ * @template T
+ * @typedef {import('./base-types.js').flatten<T>} flatten
+ */
+/**
+ * @template T
+ * @typedef {import("./base-types.js").t<T>} t
+ */
+/**
+ * @template {ObjectShape} S
+ * @typedef {import('./base-types.js').objectType<S>} objectType
+ */
+/**
+ * @template {ClassShape} T
+ * @typedef {import('./base-types.js').classType<T>} classType
+ */
+/**
+ * @template T
+ * @typedef {import("./types.js").ObjectType<T>} ObjectType
+ */
+
+/**
+ * @typedef {ClassShape | ObjectShape | ObjectType<any>} AnyObjectShape
+ */
